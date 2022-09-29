d81d6584b4d64e01dce6ab64bc3f7e6e553cd6d1
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-09-29 09:39:22 +0700

    update halaman pengajuan dan ruangan

--- functions/js/pengajuan/pengajuan.js
+++ functions/js/pengajuan/pengajuan.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
             })
             
             // EVENT KETIK TOMBOL UBAH DI KLIK
-            const btn_ubah = document.querySelectorAll('#btn-ubah');
+            const btn_ubah = document.querySelectorAll('#btn-ubah_pengajuan');
             btn_ubah.forEach(el => {
                 el.addEventListener('click', function(){
                     let id = this.dataset.id;
@@ -35,7 +35,7 @@ $(document).ready(function(){
             })
 
             // EVENT KETIKA TOMBOL DELETE DI KLIK
-            const btn_del = document.querySelectorAll('#btn-hapus');
+            const btn_del = document.querySelectorAll('#btn-hapus_pengajuan');
             const popup = document.querySelector('.modal-popup-del');
             const textPopup = popup.querySelector('#textConfirm');
 
@@ -235,13 +235,13 @@ $(document).ready(function(){
                 <td>${response.tanggal}</td>
                 <td>${response.status}</td>
                 <td class="aksi">
-                    <button class='btn btn-outline-primary btn-sm btn-edit' data-id="${response.id_pengajuan}" id="btn-ubah">
+                    <button class='btn btn-outline-primary btn-sm btn-edit' data-id="${response.id_pengajuan}" id="btn-ubah_pengajuan">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                         </svg>
                     </button>
-                    <button class='btn btn-outline-danger btn-sm btn-hapus' data-id="${response.id_pengajuan}" id='btn-hapus'>
+                    <button class='btn btn-outline-danger btn-sm btn-hapus' data-id="${response.id_pengajuan}" id='btn-hapus_pengajuan'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                         </svg>
